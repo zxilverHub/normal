b65d7b60eb16bc7693a35d11e1cbf6a4d9762815
Silver Dave Ramos <zilvdaveramos@gmail.com>
2023-08-01 17:29:47 +0800

    try natin hello lang

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,40 @@ import React, { useEffect, useState } from 'react'
 function App() {
   const [data, setData] = useState(null)
 
-  useEffect(()=>{
-    async function fetchData(){
-      try {
-        const res = await fetch(`https://restcountries.com/v3.1/all`)
-        const datas = await res.json();
-        setData(datas)
+  // useEffect(()=>{
+  //   async function fetchData(){
+  //     try {
+  //       const res = await fetch(`https://restcountries.com/v3.1/all`)
+  //       const datas = await res.json();
+  //       setData(datas)
 
-      } catch (error) {
-        console.log(error)
-      }
-    }
+  //     } catch (error) {
+  //       console.log(error)
+  //     }
+  //   }
 
-    fetchData();
-  }, [])
+  //   fetchData();
+  // }, [])
 
-  if(!data) {
-    return(
-      <p> No data... </p>
-    )
-  } else {
-    return (
-      <div>
-        { data.map((d, i)=>(
-          <p key={i}>{d?.name?.common}</p>
-        )) }
-      </div>
-    )
-  }
+  // if(!data) {
+  //   return(
+  //     <p> No data... </p>
+  //   )
+  // } else {
+  //   return (
+  //     <div>
+  //       <p>HEll</p>
+  //       { data.map((d, i)=>(
+  //         <p key={i}>{d?.name?.common}</p>
+  //       )) }
+  //     </div>
+  //   )
+  // }
+  return (
+    <>
+      <p>hello</p>
+    </>
+  )
   
  
 }
